cafff7860c6d0d9612edd5b7f4d0e95ddb210f81
Samy CHOUAM <sachouam@e2r9p19.clusters.42paris.fr>
2021-11-28 01:34:36 +0100

    main de test la suite

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: sachouam <sachouam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 21:33:00 by sachouam          #+#    #+#             */
-/*   Updated: 2021/11/28 00:17:27 by sachouam         ###   ########.fr       */
+/*   Updated: 2021/11/28 01:28:10 by sachouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,33 @@ class Test
         void bar(void);
 };
 
-Test::Test( void )
+Test::Test(void)
 {
     std::cout << "Constructor called" << std::endl;
+    this->nb = 100;
+    this->bar();
 }
 
-Test::~Test( void )
+Test::~Test(void)
 {
     std::cout << "Destructor called" << std::endl;
 }
 
+void    Test::bar(void)
+{
+    std::cout << "Member function called bar" << std::endl;
+}
+
 int
-    main( void )
+    main(void)
 {
     Test    essaie;    
     char    buf[255];
 
+
     std::cout << "PTIT TEST AU CALME" << std::endl << "UN AUTRE TEST" << std::endl;
+    std::cout << essaie.nb << std::endl;
+    //essaie.bar();
     std::cout << "Put on input please : ";
     std::cin >> buf;
 	std::cout << "Here is you input : " << buf << "" << std::endl;
