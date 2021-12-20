93975db0df4a633368e0c4eba1e5535a23c82388
Claken <samychouam@gmail.com>
2021-12-21 00:07:47 +0100

    debut CPP03 ex00

---
+++ CPP_Module_03/ex00/ClapTrap.cpp
@@ -0,0 +1,37 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "Constructor called" << std::endl;
+	this->_name = name;
+	this->_hitPoints = 10;
+	this->_energyPoints = 10;
+	this->_attackDamage = 0;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void	ClapTrap::attack(std::string const & target)
+{
+	std::cout << "ClapTrap " << this->_name;
+	std::cout << " attack " << target;
+	std::cout << ", causing " this->_attackDamage;
+	std::cout << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->_name;
+	std::cout << " took " << amount;
+	std::cout << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->_name;
+	std::cout << " regained " << amount;
+	std::cout << " points of energy!" << std::endl;
+}
---
+++ CPP_Module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,32 @@
+#ifndef CLAP_TRAP_HPP
+
+# define CLAP_TRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	private :
+
+	std::string		_name;
+	int				_hitPoints;
+	int				_energyPoints;
+	int				_attackDamage;
+
+	public :
+
+	ClapTrap(void) {};
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const & instance);
+	~ClapTrap(void);
+
+	void			attack(std::string const & target);
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
+
+	ClapTrap& operator=(ClapBack const & instance);
+
+};
+
+#endif
+
