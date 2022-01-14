f7f2744196902e7a3ee1ad81018de7011d81d636
Claken <samychouam@gmail.com>
2022-01-14 21:35:48 +0100

    CCP06 ex00 : en progression

--- CPP_Module_06/ex00/main.cpp
+++ CPP_Module_06/ex00/main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
+#include <math.h>
+#include <climits>
 
 int
 	main(int ac, char **av)
 {
 	if (ac != 2)
 		return (-1);
+	std::string str = av[1];
+	std::istringstream iss(av[1]);
+	if (iss.fail())
+		return (-1);
 
-	std::istringstream iss (av[1]);
-	//int		conv;
-	float conv = std::stof(iss);
-	//iss >> conv;
+	double conv;
 
-	//std::cout << conv << std::endl;
+	if (str == "nan" || str == "nanf")
+		conv = NAN;
+	else if (str == "inf" || str == "inff"
+	|| str == "+inf" || str == "+inff")
+		conv = INFINITY;
+	else if (str == "-inf" || str == "-inff")
+		conv = -INFINITY;
+	else
+		iss >> conv;
 
 	char	a = static_cast<char>(conv);
 	int		b = static_cast<int>(conv);
 	float	c = static_cast<float>(conv);
 	double	d = static_cast<double>(conv);
-	std::cout << "char   : " << a << std::endl;
-	std::cout << "int    : " << b << std::endl;
-	std::cout << "float  : " << c << std::endl;
-	std::cout << "double : " << d << std::endl;
+	std::cout << "char   : ";
+	if (a > 32)
+		std::cout << a << std::endl;
+	else
+		std::cout << "Non displayable" << std::endl;
+	std::cout << "int    : ";
+	if (b != INT_MIN)
+		std::cout << b << std::endl;
+	else
+		std::cout << "impossible" << std::endl;
+	std::cout << "float  : " << std::fixed << std::setprecision(1) << c << "f" << std::endl;
+	std::cout << "double : " << std::fixed << std::setprecision(1) << d << std::endl;
 	return 0;
 }
