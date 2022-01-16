cf8df4aad19fc0f20f6450993c6dee6d8f705288
Claken <samychouam@gmail.com>
2022-01-16 15:36:47 +0100

    CCP06 ex02 : normalement fini

--- CPP_Module_06/ex02/main.cpp
+++ CPP_Module_06/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <exception>
 
 class Base
 {
@@ -16,34 +17,64 @@ Base*	generate(void)
 	srand((unsigned)time(0));
 	int i = rand() % 3;
 	if (i == 0)
+	{
 		return (new A());
+	}
 	else if (i == 1)
+	{
 		return (new B());
+	}
 	else
+	{
 		return (new C());
+	}
 	return (NULL);
 }
 
 void	identify(Base *p)
 {
-	(void)p;
-	std::cout << "A" << std::endl;
-	std::cout << "B" << std::endl;
-	std::cout << "C" << std::endl;
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
 }
 
 void	identify(Base& p)
 {
-	(void)p;
-	std::cout << "A" << std::endl;
-	std::cout << "B" << std::endl;
-	std::cout << "C" << std::endl;
+	try
+	{
+		A & castA = dynamic_cast<A &>(p);
+		(void)castA;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception &bc) {}
+	try
+	{
+		B & castB = dynamic_cast<B &>(p);
+		(void)castB;
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception &bc) {}
+	try
+	{
+		C & castC = dynamic_cast<C &>(p);
+		(void)castC;
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception &bc) {}
 }
 
 int		main(void)
 {
-	Base *inst = generate();
+	Base	*inst1 = generate();
+	Base&	inst2 = *inst1;
 
-	delete inst;
+	std::cout << "identify (pointer)   : ";
+	identify(inst1);
+	std::cout << "identify (reference) : ";
+	identify(inst2);
+	delete inst1;
 	return (0);
 }
