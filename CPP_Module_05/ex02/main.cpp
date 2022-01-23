eb935513997abee060326393f2eec4c56723e9a2
Claken <samychouam@gmail.com>
2022-01-23 01:41:00 +0100

    CPP05 : modifs

--- CPP_Module_05/ex02/main.cpp
+++ CPP_Module_05/ex02/main.cpp
@@ -9,38 +9,53 @@ int
 	main(void)
 {
 	Bureaucrat				Bob("Bob", 1);
+	Bureaucrat				Lame("Lame", 149);
 	ShrubberyCreationForm	shrubberyForm("SCForm");
 	PresidentialPardonForm	presidentialForm("PPForm");
 	RobotomyRequestForm		robotomizeForm("RRForm");
+	std::cout << Bob << std::endl;
+	std::cout << Lame << std::endl;
+	std::cout << std::endl;
 
 	Bob.executeForm(shrubberyForm);
+	Lame.signForm(shrubberyForm);
 	Bob.signForm(shrubberyForm);
+	Lame.executeForm(shrubberyForm);
 	Bob.executeForm(shrubberyForm);
 	std::cout << std::endl;
 
 	Bob.executeForm(presidentialForm);
+	Lame.signForm(presidentialForm);
 	Bob.signForm(presidentialForm);
+	Lame.executeForm(presidentialForm);
 	Bob.executeForm(presidentialForm);
 	std::cout << std::endl;
 
 	Bob.executeForm(robotomizeForm);
+	Lame.signForm(robotomizeForm);
 	Bob.signForm(robotomizeForm);
+	Lame.executeForm(robotomizeForm);
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	sleep(1);
-	Bob.executeForm(robotomizeForm);
-	sleep(1);
+	std::cout << std::endl;
 	Bob.executeForm(robotomizeForm);
 	return (0);
 }
