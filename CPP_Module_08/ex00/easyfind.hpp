7bccc7038f2ecbf2498642e1f70825e536137c78
Samy CHOUAM <sachouam@e1r4p4.42.fr>
2022-01-23 18:00:43 +0100

    CPP08 : ex00 normalement fini

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -3,5 +3,19 @@
 #include <iostream>
 #include <exception>
 #include <list>
+#include <vector>
 
 template<typename T>
+int    easyfind(T const & lst, int nb)
+{
+    typename T::const_iterator it;
+
+    it = lst.begin();
+    while (it != lst.end())
+    {
+        if (*it == nb)
+            return (*it);
+        ++it;
+    }
+    throw (std::exception());
+}
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -4,11 +4,46 @@ int
 	main(void)
 {
 	std::list<int>		list1;
+	std::vector<int>	vect1;
 
 	list1.push_back(75);
 	list1.push_back(45);
 	list1.push_back(12);
 
-	std::cout << "easyfind(list, 12) : " << easyfind(list1, 12) << std::endl;
+	vect1.push_back(75);
+	vect1.push_back(45);
+	vect1.push_back(12);
+
+	std::cout << "easyfind(list, 12)  : " << easyfind(list1, 12) << std::endl;
+	std::cout << "easyfind(list, 75)  : " << easyfind(list1, 75) << std::endl;
+	std::cout << "easyfind(list, 45)  : " << easyfind(list1, 45) << std::endl;
+
+	try
+	{
+		std::cout << "easyfind(list, 10)  : ";
+		int nb = easyfind(list1, 10);
+		std::cout << nb << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << "easyfind(vect, 12)  : " << easyfind(vect1, 12) << std::endl;
+	std::cout << "easyfind(vect, 75)  : " << easyfind(vect1, 75) << std::endl;
+	std::cout << "easyfind(vect, 45)  : " << easyfind(vect1, 45) << std::endl;
+
+	try
+	{
+		std::cout << "easyfind(vect, -10) : ";
+		int nb = easyfind(vect1, -10);
+		std::cout << nb << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
 	return (0);
 }
